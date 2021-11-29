36bbfaf0fbb67fb9e4d60a8c7f250a20d5d06eca
Murilo <murilo.pereira.botelho@gmail.com>
2021-11-29 14:21:52 -0300

    Setup: Firebase and router

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlkEG3QeTVFAjZg5_S4pKP3p2XdB0f3hw",
+  authDomain: "fireship-next-js-b42c6.firebaseapp.com",
+  projectId: "fireship-next-js-b42c6",
+  storageBucket: "fireship-next-js-b42c6.appspot.com",
+  messagingSenderId: "748027436583",
+  appId: "1:748027436583:web:b743e89a550b87a9a23802",
+  measurementId: "G-CW26D7RDDP",
+};
+
+if (!firebase.apps.length) {
+  // Just cause in development next might start twitce and will throw error
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const firestore = firebase.firestore();
---
+++ pages/[username]/[slug].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UsernameSlug() {
+  return <div></div>;
+}
